bf03eeab2fd01f4a653ccba58e382f0dd6a47001
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-11 10:05:22 -0300

    add gitignore

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# prompt-engineering
+# Prompt engineering
---
+++ ab-evaluation/main.py
@@ -0,0 +1,5 @@
+promptA = """Product description: A pair of shoes that can
+             fit any foot size.
+             Seed words: adaptable, fit, omni-fit.
+             Product names:"""
+print(promptA)
