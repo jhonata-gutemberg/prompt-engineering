795f9b29b6d6e559bd7136b224f8a72bca2dcfc5
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-12 10:04:40 -0300

    add openai dependency

--- ab-evaluation/main.py
+++ ab-evaluation/main.py
@@ -1,5 +1,26 @@
-promptA = """Product description: A pair of shoes that can
-             fit any foot size.
-             Seed words: adaptable, fit, omni-fit.
-             Product names:"""
-print(promptA)
+from openai import OpenAI
+import os
+
+promptA = """Product description: A pair of shoes that can fit any foot size.
+Seed words: adaptable, fit, omni-fit.
+Product names:"""
+
+promptB = """Product description: A home milkshake maker.
+Seed words: fast, healthy, compact.
+Product names: HomeShaker, Fit Shaker, QuickShake, ShakeMaker
+
+Product description: A watch that can tell accurate time in space.
+Seed words: astronaut, space-hardened, eliptical orbit
+Product names: AstroTime, SpaceGuard, Orbit-Accurate, EliptoTime.
+
+Product description: A pair of shoes that can fit any footsize.
+Seed words: adaptable, fit, omni-fit.
+Product names:"""
+
+testPrompts = [promptA, promptB]
+
+client = OpenAI(
+    api_key=os.environ['OPENAI_API_KEY']
+)
+
+print(client)
